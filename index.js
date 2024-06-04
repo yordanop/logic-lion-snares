63455740c713614b5e19720947dfe989d0fead31
Eynar <yordanopv@gmail.com>
2024-06-04 17:34:49 -0600

    set selection of shape

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require("inquirer");
+const Circle = require('./lib/circle.js')
+const Triangle = require('./lib/triangle.js')
+const Square = require('./lib/square.js')
 const fs = require("fs");
 
 // Array with needed questions
@@ -38,8 +41,23 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt(questions).then((answers) => {
-    writeToFile('README', answers)
+inquirer.prompt(questions).then(({text, shape, backgorund, textColor}) => {
+    let shapeLogo;
+    switch(shape){
+        case "Circle":
+            shape = new Circle();
+            break;
+        case "Triangle":
+            shape = new Triangle();
+            break;
+        case "Square":
+            shape = new Square();
+            break;
+        default:
+            console.log("That's what you want it? You kill this app :(")
+            break;
+    }
+    shape.set
   });
 }
 
