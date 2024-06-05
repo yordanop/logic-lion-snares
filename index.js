629036a0cccfe162bea86c0837f148c80d425f7b
Eynar <yordanopv@gmail.com>
2024-06-04 18:45:47 -0600

    creating svg file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require("inquirer");
-const Circle = require('./lib/circle.js')
-const Triangle = require('./lib/triangle.js')
-const Square = require('./lib/square.js')
+const Circle = require('./lib/circle.js');
+const Triangle = require('./lib/triangle.js');
+const Square = require('./lib/square.js');
+const Logo = require('./lib/logo.js');
 const fs = require("fs");
 
 // Array with needed questions
@@ -32,11 +33,7 @@ const questions = [
 
 function writeToFile(fileName, data) {
     
-    fs.writeFile(`${data.title.replace(/\s+/g, '_')}_${fileName}.md`, readMeCreator.generateMarkdown(data), (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
+    
 }
 
 // TODO: Create a function to initialize app
@@ -45,19 +42,31 @@ inquirer.prompt(questions).then(({text, shape, backgorund, textColor}) => {
     let shapeLogo;
     switch(shape){
         case "Circle":
-            shape = new Circle();
+            shapeLogo = new Circle();
             break;
         case "Triangle":
-            shape = new Triangle();
+            shapeLogo = new Triangle();
             break;
         case "Square":
-            shape = new Square();
+            shapeLogo = new Square();
             break;
         default:
             console.log("That's what you want it? You kill this app :(")
             break;
     }
-    shape.set
+    shapeLogo.setColor(backgorund);
+
+    const svgCreate = new Logo();
+
+    svgCreate.setText(text, textColor);
+    svgCreate.getShape(shapeLogo);
+
+    fs.writeFile(`./examples/${text}_logo.svg`, svgCreate.render(), (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+
   });
 }
 
