cf484338b6b22cc31dc03dd76176d0be9de13193
Eynar <yordanopv@gmail.com>
2024-06-05 21:31:12 -0600

    set max 3 char for text logo

---
+++ README.md
@@ -0,0 +1,47 @@
+# Logo Generator
+  
+  ## Description
+
+  This is an application run on command to generate svg files with a simple logo
+
+  ## Questions
+
+  My email : yordanopv@gmail.com
+  
+  My Github page : https://github.com/yordanop
+
+  ## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+You need npm and from npm you need to install inquirer
+
+## Usage
+
+the app will ask for the basic info it need to generate the files
+
+## Credits
+
+my instructor
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+   [MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Features
+
+simple and light application
+
+## How to Contribute
+
+Contact me
+
+  
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// adding packages and objects needed
 const inquirer = require("inquirer");
 const Circle = require('./lib/circle.js');
 const Triangle = require('./lib/triangle.js');
 const Square = require('./lib/square.js');
 const Logo = require('./lib/logo.js');
 const fs = require("fs");
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+
+// define max length of prompts/answers
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 // Array with needed questions
 const questions = [
     {
-        type: "input",
+        type: "maxlength-input",
         message: "Type 3 characters for the logo",
-        name: "text"
+        name: "text",
+        maxLength: 3
     },
     {
         type: "list",
@@ -33,7 +39,9 @@ const questions = [
 
 // TODO: Create a function to initialize app
 function init() {
+    // First inquirer will make the questions needed to create the logo
 inquirer.prompt(questions).then(({text, shape, backgorund, textColor}) => {
+    // depending on the shape selected it creates a new instance of the chosen shape
     let shapeLogo;
     switch(shape){
         case "Circle":
@@ -49,8 +57,10 @@ inquirer.prompt(questions).then(({text, shape, backgorund, textColor}) => {
             console.log("That's what you want it? You kill this app :(")
             break;
     }
+
     shapeLogo.setColor(backgorund);
 
+    // creating a new isntance of logo that will receive th answers of the text and for the color of the text
     const svgCreate = new Logo();
 
     svgCreate.setText(textColor, text);
@@ -60,6 +70,7 @@ inquirer.prompt(questions).then(({text, shape, backgorund, textColor}) => {
         if (error) {
           console.log(error);
         }
+        console.log('Generated logo.svg');
       });
 
   });
